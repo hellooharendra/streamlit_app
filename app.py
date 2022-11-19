3dbd315b6794d0e81ba2031b003fff6e839b36c7
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-11-19 23:11:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.title('Uber pickups in NYC')
-st.header('this is harendra')
+st.title('Hello everyone')
+st.header('this is on testing mode @powered By:Harendra Prajapati')
